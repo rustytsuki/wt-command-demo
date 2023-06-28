1a8856f2285bbc1a594ac93071448cc74afc4a1f
RustyTsuki <rustytsuki@gmail.com>
2023-06-28 12:06:07 +0800

    add command

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wt-command-demo
-Thist a demo of a cmd file to test windows terminal wt command
+This is a demo of a bat file to test windows terminal wt command
+
+you must install [windows terminal](https://github.com/microsoft/terminal) first. you can refer to [wt command](https://learn.microsoft.com/en-us/windows/terminal/command-line-arguments?tabs=windows)
+
+and run /bat/demo.bat in windows terminal. or set the windows terminal as default terminal and double click the demo.bat by mouse.
+
---
+++ bat/demo.bat
@@ -0,0 +1,20 @@
+wt -w 0 nt --title red --tabColor #FF0000 "%~dp0\pause.bat"
+timeout 2
+wt -w 0 sp --title red --tabColor #FF0000 "%~dp0\pause.bat"
+timeout 2
+
+wt -w 0 nt --title green --tabColor #00FF00 "%~dp0\pause.bat"
+timeout 2
+wt -w 0 sp --title green --tabColor #00FF00 "%~dp0\pause.bat"
+timeout 2
+wt -w 0 mf left
+timeout 2
+wt -w 0 sp -v --title green --tabColor #00FF00 "%~dp0\pause.bat"
+timeout 2
+wt -w 0 mf right
+timeout 2
+wt -w 0 sp -v --title green --tabColor #00FF00 "%~dp0\pause.bat"
+timeout 2
+
+wt -w 0 nt --title blue --tabColor #0000FF "%~dp0\pause.bat"
+timeout 2
